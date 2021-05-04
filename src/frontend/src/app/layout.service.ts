5b6087c69d8ee56311da626be34d53324bfba0d4
Gergő Fehér <gfeher@opswat.com>
2021-05-05 00:19:33 +0200

    frontend: Save layout in designer and query layouts in the new Layout page
    23

---
+++ src/frontend/src/app/layout.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+
+const LAYOUT_API = 'http://localhost:4200/api/v1/layouts';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+
+  constructor(private http: HttpClient) { }
+
+  getLayouts(): Observable<any> {
+    return this.http.get(LAYOUT_API, httpOptions);
+  }
+
+  saveLayout(coords: string): Observable<any> {
+    return this.http.post(LAYOUT_API, {coords: coords}, httpOptions);
+  }
+}
