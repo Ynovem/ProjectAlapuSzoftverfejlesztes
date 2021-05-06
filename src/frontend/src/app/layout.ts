b4a58e9bcfbb9b05a33015193a0aa3376d006820
Gergő Fehér <gfeher@opswat.com>
2021-05-06 22:01:28 +0200

    frontend: Toggle layout coords
    23

--- src/frontend/src/app/layout.ts
+++ src/frontend/src/app/layout.ts
@@ -4,3 +4,25 @@ export interface Layout {
 	created: string;
 	coords: string;
 }
+
+export interface Displayable {
+	displayed: boolean;
+}
+
+export class LayoutDisplay implements Layout, Displayable {
+	id: number;
+	name: string;
+	coords: string;
+	created: string;
+
+	displayed: boolean;
+
+	constructor(layout: Layout) {
+		this.id = layout.id;
+		this.name = layout.name;
+		this.coords = JSON.parse(layout.coords);
+		this.created = layout.created;
+
+		this.displayed = false;
+	}
+}
