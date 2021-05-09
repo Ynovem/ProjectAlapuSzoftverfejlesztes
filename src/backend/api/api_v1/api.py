c8e3c8f6f8b2a719480014810f787ac9aa9312ab
Gergő Fehér <gfeher@opswat.com>
2021-05-09 22:49:43 +0200

    backtend: Cleanup and styling
    25

--- src/backend/api/api_v1/api.py
+++ src/backend/api/api_v1/api.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import login, users, layouts
+from api.api_v1.endpoints import layouts
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["user"])
 api_router.include_router(layouts.router, prefix="/layouts", tags=["layout"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
