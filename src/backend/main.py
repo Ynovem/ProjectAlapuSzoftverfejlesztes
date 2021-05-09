639b62c1bdf181a27129744bae1a7313deb2f946
Gergő Fehér <gfeher@opswat.com>
2021-05-09 22:49:43 +0200

    backtend: Cleanup and styling
    25

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-# from app.api.api_v1.api import api_router
 from api.api_v1.api import api_router
-# from app.core.config import settings
 from core.config import settings
 
 app = FastAPI(
