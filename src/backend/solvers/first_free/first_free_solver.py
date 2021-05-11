07434b2c7872d33da57c73df7f6973f9b12682a0
Kukoda Balazs <kukodabalazs@gmail.com>
2021-05-11 23:01:10 +0200

    Changed FirstFree class to use functions of common package

--- src/backend/solvers/first_free/first_free_solver.py
+++ src/backend/solvers/first_free/first_free_solver.py
@@ -1,10 +1,12 @@
 from schemas import Layout
 from crud import layout as crudLayout
+from solvers.common.common import Common
 
 import json
 import numpy as np
 
 DEFAULT_CONSTRAINT = 150
+
 
 class FirstFree:
     def __init__(self):
@@ -12,52 +14,27 @@ class FirstFree:
 
     def solve(self, layout: Layout):
         seats = json.loads(layout.coords)
-        self.add_num(seats) # same as index in array
-        self.add_neighbors(seats, self.get_edges(seats)) # list of conflicting seats compared to the current one
-
-        forbidden = list() # temporary list of forbidden seats
-        allowed = list()# temporary list of free seats
-
-        for current in seats: # core of algorithm
+        # num is same as index of array
+        Common.add_num(seats)
+        # create list of conflicting seats compared to the current one
+        Common.add_neighbors(seats, Common.get_edges(seats, DEFAULT_CONSTRAINT))
+        # temporary list of forbidden seats
+        forbidden = list()
+        # temporary list of free seats
+        allowed = list()
+        # core of algorithm
+        for current in seats:
             if current["num"] not in forbidden:
                 allowed.append(current["num"])
                 forbidden.extend(current["neighbors"])
-
-        for seat in seats: # set busy
+        # set busy
+        for seat in seats:
             if seat["num"] in allowed:
                 seat["busy"] = True
             else:
                 seat["busy"] = False
-            del seat["neighbors"] # delete unnecessary properties
-            del seat["num"] # delete unnecessary properties
+            # delete unnecessary properties
+            del seat["neighbors"]
+            del seat["num"]
         layout.coords = json.dumps(seats)
         return layout
-
-    def print_seats(self, seats):
-        for seat in seats:
-            print(seat)
-
-    def add_num(self, seats):
-        for i in range(len(seats)):
-            seats[i]["num"] = i
-
-    def get_edges(self, seats):
-        ret = list()
-        i = 0
-        while i < len(seats):
-            j = i + 1
-            while j < len(seats):
-                if np.linalg.norm( np.array( (seats[i]["x"], seats[i]["y"]) ) - np.array( (seats[j]["x"], seats[j]["y"]) ) ) <= DEFAULT_CONSTRAINT:
-                    ret.append((i,j))
-                j = j + 1
-            i = i + 1
-        return ret
-
-    def add_neighbors(self, seats, edges):
-        for seat in seats:
-            seat["neighbors"] = list()
-        for edge in edges:
-            if edge[1] not in seats[edge[0]]["neighbors"]:
-                seats[edge[0]]["neighbors"].append(edge[1])
-            if edge[0] not in seats[edge[1]]["neighbors"]:
-                seats[edge[1]]["neighbors"].append(edge[0])
