cc868af837d26a30aa9093c6f99b06adcc5c8f53
Martin <erdely.martin@gmail.com>
2021-05-13 20:13:51 +0200

    Fabric service, Designer generate seats, canvas selection styles and snap to grid functionality

--- src/frontend/src/app/_services/fabric.service.ts
+++ src/frontend/src/app/_services/fabric.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
 
 import { fabric } from 'fabric';
+import {IEvent, Point} from 'fabric/fabric-impl';
 
 export class POINT {
   x: number;
@@ -14,84 +15,289 @@ export class POINT {
 @Injectable({
   providedIn: 'root'
 })
-export class FabricServiceService {
-  // draw properties are public to keep the demo more compact; but, you break it ... you buy it
-  public strokeWidth: number;
-  public strokeColor: string;
-  public circleRadius: number;
-  public circleFill: string;
+export class FabricService {
+  protected canvas?: fabric.Canvas;
+  protected width = 0;
+  protected height = 0;
+  protected cellSize = 10;
+  private isDragging = false;
+  private isHovering = false;
+  private lastPosX = -1;
+  private lastPosY = -1;
+  private snapToGrid = true;
 
-  protected _canvas: fabric.Canvas;
-
-  protected _points: Array<fabric.Circle>;
-  protected _polylines: Record<string, fabric.Polyline>;
-
-  constructor() {
-    this.strokeWidth  = 2;
-    this.strokeColor  = '#000000';
-    this.circleFill   = '#0000ff';
-    this.circleRadius = 2;
-
-    this._points    = new Array<fabric.Circle>();
-    this._polylines = {};
+  constructor(protected ngZone: NgZone) {
+    fabric.Group.prototype.lockScalingX = true;
+    fabric.Group.prototype.lockScalingY = true;
+    fabric.Group.prototype.transparentCorners = false;
+    fabric.Group.prototype.borderColor = 'rgba(30, 136, 229, 0.9)';
+    fabric.Group.prototype.cornerColor = 'rgba(30, 136, 229, 0.9)';
+    fabric.Group.prototype.cornerSize = 7;
+    fabric.Group.prototype.setControlsVisibility({
+      mtr: false
+    });
+    fabric.Object.prototype.lockScalingX = true;
+    fabric.Object.prototype.lockScalingY = true;
+    fabric.Object.prototype.transparentCorners = false;
+    fabric.Object.prototype.borderColor = 'rgba(30, 136, 229, 0.9)';
+    fabric.Object.prototype.cornerColor = 'rgba(30, 136, 229, 0.9)';
+    fabric.Object.prototype.cornerSize = 7;
+    fabric.Object.prototype.setControlsVisibility({
+      ml: false,
+      mt: false,
+      mr: false,
+      mb: false,
+      mtr: false
+    });
+    fabric.Canvas.prototype.notAllowedCursor = 'default';
   }
 
-  public set canvas(surface: fabric.Canvas) {
-    this._canvas = surface;
+  public set Canvas(canvas: fabric.Canvas) {
+    this.canvas = canvas;
+    this.canvas.on('object:moving', event => {
+      this.onObjectMove(event);
+    });
   }
 
+  public get Canvas(): fabric.Canvas {
+    if (this.canvas) { return this.canvas; }
+    else { return new fabric.Canvas(''); }
+  }
+
+  public set Width(width: number) {
+    this.width = width;
+  }
+
+  public set Height(height: number) {
+    this.height = height;
+  }
+
+  public set SnapToGrid(value: boolean) {
+    this.snapToGrid = value;
+  }
 
   public clear(): void {
-    if (this._canvas) {
-      this._points.forEach((circle: fabric.Circle): void => {
-        this._canvas.remove(circle);
-      });
-
-      this._points.length = 0;
-
-      Object.keys(this._polylines).forEach( (name: string): void => {
-        this._canvas.remove(this._polylines[name]);
-      });
-
-      this._polylines = {};
-
-      this._canvas.renderAll();
-    }
-  }
-
-  public addPoint(p: POINT): void {
-    const circle: fabric.Circle = new fabric.Circle(
-      {
-        left: p.x - this.circleRadius,
-        top: p.y - this.circleRadius,
-        fill: this.circleFill,
-        radius: this.circleRadius
-      });
-
-    this._points.push(circle);
-
-    if (this._canvas) {
-      this._canvas.add(circle);
-      this._canvas.renderAll();
-    }
-  }
-
-  public addPolyline(name: string, points: Array<POINT>, clear: boolean = true): void {
-    const polyLine: fabric.Polyline = new fabric.Polyline(points, {
-      strokeWidth: this.strokeWidth,
-      stroke: this.strokeColor,
-      fill: 'transparent',
+    if (!this.canvas) { return; }
+    this.canvas.getObjects().filter(obj => obj.evented === true).forEach(obj => {
+      if (!this.canvas) { return; }
+      this.canvas.remove(obj);
     });
+  }
 
-    if (this._canvas) {
-      if (clear && this._polylines[name] !== undefined) {
-        this._canvas.remove(this._polylines[name]);
+  generateSeats(rows: number, cols: number, distanceX: number, distanceY: number, seatSize: number): void{
+    if (!this.canvas) { return; }
+    this.clear();
+    for (let row = 0; row < rows; row++){
+      for (let col = 0; col < cols; col++){
+        const seat = new fabric.Rect({
+          width: seatSize,
+          height: seatSize,
+          fill: '#ffda79',
+          hasBorders: true,
+          stroke : '#ccae62',
+          strokeWidth : 1,
+          opacity: 1,
+          lockScalingX: true,
+          lockScalingY: true,
+          top: this.cellSize + row * (distanceY + seatSize),
+          left: this.cellSize + col * (distanceX + seatSize)
+        });
+        this.canvas.add(seat);
+      }
+    }
+    this.canvas.renderAll();
+  }
+
+  drawGrid(): void {
+    if (!this.canvas) { return; }
+    const gridWidth = this.width * 2;
+    const gridHeight = this.width * 2;
+
+    const gridLen = gridWidth / this.cellSize;
+    for (let i = 0; i < gridLen; i++) {
+      const distance = i * this.cellSize;
+      const vertical = new fabric.Line([distance, 0, distance, gridHeight], {
+        stroke: '#ebebeb',
+        strokeWidth: 1,
+        selectable: false,
+        evented: false
+      });
+      const horizontal = new fabric.Line([0, distance, gridWidth, distance], {
+        stroke: '#ebebeb',
+        strokeWidth: 1,
+        selectable: false,
+        evented: false
+      });
+      if (distance <= gridHeight) {
+        this.canvas.add(horizontal);
+      }
+      this.canvas.add(vertical);
+      if (i % 5 === 0) {
+        horizontal.set({stroke: '#cccccc'});
+        vertical.set({stroke: '#cccccc'});
+        if (distance <= gridHeight) {
+          this.canvas.sendBackwards(horizontal);
+        }
+        this.canvas.sendBackwards(vertical);
+      } else {
+        if (distance <= gridHeight) {
+          this.canvas.sendToBack(horizontal);
+        }
+        this.canvas.sendToBack(vertical);
+      }
+    }
+    this.canvas.renderAll();
+  }
+
+  onScroll(event: WheelEvent): void {
+      if (!this.canvas) { return; }
+      const delta = event.deltaY;
+      let zoom = this.canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 8) {
+        zoom = 8;
+      }
+      if (zoom < 0.5) {
+        zoom = 0.5;
       }
 
-      this._canvas.add(polyLine);
-      this._canvas.renderAll();
-    }
+      this.canvas.zoomToPoint({x: event.offsetX, y: event.offsetY} as Point, zoom);
 
-    this._polylines[name] = polyLine;
+      if (event.deltaY > 0) {
+        const vpt = this.canvas.viewportTransform;
+        if (vpt) {
+          if (vpt[4] > 0) {
+            vpt[4] = 0;
+          }
+          if (vpt[4] < -2 * this.width * zoom + this.width) {
+            vpt[4] = Math.floor(-2 * this.width * zoom + this.width);
+          }
+          if (vpt[5] > 0) {
+            vpt[5] = 0;
+          }
+          if (vpt[5] < -2 * this.height * zoom + this.height) {
+            vpt[5] = Math.floor(-2 * this.height * zoom + this.height);
+          }
+        }
+      }
+
+      this.canvas.forEachObject(object => {
+        object.setCoords();
+      });
+      this.canvas.requestRenderAll();
+  }
+
+  onMouseOver(event: MouseEvent): void{
+    this.isHovering = true;
+  }
+
+  onMouseLeave(event: MouseEvent): void{
+    this.isHovering = false;
+  }
+
+  onKeyDown(event: KeyboardEvent): void{
+    if (!this.canvas) { return; }
+    if (event.key === 'Alt'){
+      event.preventDefault();
+      if (this.isHovering){
+        this.canvas.setCursor('pointer');
+        this.canvas.defaultCursor = 'pointer';
+        this.canvas.hoverCursor = 'pointer';
+      }
+    }
+  }
+
+  onKeyUp(event: KeyboardEvent): void{
+    if (event.key === 'Alt') {
+      if (!this.canvas) { return; }
+      if (this.isDragging){
+        if (!this.canvas) { return; }
+        this.isDragging = false;
+        this.canvas.forEachObject(object => {
+          object.setCoords();
+        });
+        setTimeout(this.enableCanvasSelection, 0, this.canvas);
+      }
+      this.canvas.setCursor('default');
+      this.canvas.defaultCursor = 'default';
+      this.canvas.hoverCursor = 'move';
+    }
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    if (event.altKey) {
+      if (!this.canvas) { return; }
+      this.canvas.discardActiveObject();
+      this.isDragging = true;
+      this.canvas.selection = false;
+      this.lastPosX = event.clientX;
+      this.lastPosY = event.clientY;
+    }
+  }
+
+  onMouseMove(event: MouseEvent): void {
+    if (this.isDragging) {
+      if (!this.canvas) { return; }
+      const zoom = this.canvas.getZoom();
+      const distanceX = event.clientX - this.lastPosX;
+      const distanceY = event.clientY - this.lastPosY;
+      const vpt = this.canvas.viewportTransform;
+      if (vpt) {
+        vpt[4] += distanceX;
+        if (vpt[4] > 0) {
+          vpt[4] = 0;
+        }
+        if (vpt[4] < -2 * this.width * zoom + this.width) {
+          vpt[4] = Math.floor(-2 * this.width * zoom + this.width);
+        }
+        if (vpt[4] <= 0 && vpt[4] >= -2 * this.width * zoom + this.width) {
+          this.lastPosX = event.clientX;
+        }
+
+        vpt[5] += distanceY;
+        if (vpt[5] > 0) {
+          vpt[5] = 0;
+        }
+        if (vpt[5] < -2 * this.height * zoom + this.height) {
+          vpt[5] = Math.floor(-2 * this.height * zoom + this.height);
+        }
+        if (vpt[5] <= 0 && vpt[5] >= -2 * this.height * zoom + this.height) {
+          this.lastPosY = event.clientY;
+        }
+      }
+      this.canvas.requestRenderAll();
+    }
+  }
+
+  onMouseUp(event: MouseEvent): void {
+    if (this.isDragging){
+      if (!this.canvas) { return; }
+      this.isDragging = false;
+      this.canvas.forEachObject(object => {
+        object.setCoords();
+      });
+      setTimeout(this.enableCanvasSelection, 0, this.canvas);
+    }
+  }
+
+  enableCanvasSelection(canvas: fabric.Canvas): void {
+    canvas.selection = true;
+  }
+
+  onObjectMove(event: IEvent): void {
+    if (!this.snapToGrid) { return; }
+    this.ngZone.runOutsideAngular( () => {
+      const object = event.target;
+      if (!object || !object.top || !object.left) { return; }
+      if (object.top % this.cellSize !== 0 || object.left % this.cellSize !== 0){
+        object.top = this.roundWhenNearEdge(object.top / this.cellSize) * this.cellSize;
+        object.left = this.roundWhenNearEdge(object.left / this.cellSize) * this.cellSize;
+      }
+      object.setCoords();
+    });
+  }
+
+  roundWhenNearEdge(num: number): number {
+    if (num % 1 >= 0.75) { return Math.round(num); } else { return Math.floor(num); }
   }
 }
--- src/frontend/src/app/designer/designer.component.ts
+++ src/frontend/src/app/designer/designer.component.ts
@@ -1,27 +1,110 @@
-import {Component, Directive, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit,
+  Component, ElementRef, HostListener,
+  Input, NgZone,
+  OnInit,
+} from '@angular/core';
 
 import { LayoutService} from '../_services/layout.service';
 
 import { Metric, OptSeat } from '../_dtos/seatmap';
+import {FabricService} from '../_services/fabric.service';
+import {fabric} from 'fabric';
 
 @Component({
   selector: 'app-designer',
   templateUrl: './designer.component.html',
   styleUrls: ['./designer.component.scss']
 })
-export class DesignerComponent implements OnInit{
+export class DesignerComponent implements OnInit, AfterViewInit{
+  canvasId = 'designer-canvas';
   map: OptSeat[][] = [];
   name = '';
   json: object[] = [];
   @Input() rowMetric: Metric = new Metric();
   @Input() colMetric: Metric = new Metric();
+  @Input() snapToGrid = true;
 
-  constructor(private layoutService: LayoutService) {
+  constructor(private layoutService: LayoutService, private fabricService: FabricService,
+              private ngZone: NgZone, private elementRef: ElementRef) {
     document.body.style.overflowY = 'hidden';
   }
 
   ngOnInit(): void
   {}
+
+  ngAfterViewInit(): void {
+    this.fabricService.Canvas = new fabric.Canvas(this.canvasId, {
+      imageSmoothingEnabled: false,
+      backgroundColor: '#fff',
+      renderOnAddRemove: false,
+      selectionColor: 'rgba(116, 185, 255, 0.1)',
+      selectionBorderColor: 'rgba(6, 89, 153, 0.7)',
+      selectionLineWidth: 1.5,
+      selectionDashArray: [10, 5],
+      notAllowedCursor: 'default'
+    });
+    this.fabricService.Width = 1500;
+    this.fabricService.Height = 857;
+
+    this.elementRef.nativeElement.querySelector('.canvas-container')
+      .addEventListener('wheel', (event: WheelEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onScroll(event);
+        });
+      });
+
+    this.elementRef.nativeElement.querySelector('.canvas-container')
+      .addEventListener('mousedown', (event: MouseEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onMouseDown(event);
+        });
+      });
+
+    this.elementRef.nativeElement.querySelector('.canvas-container')
+      .addEventListener('mousemove', (event: MouseEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onMouseMove(event);
+        });
+      });
+
+    window
+      .addEventListener('mouseup', (event: MouseEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onMouseUp(event);
+        });
+      });
+
+    this.elementRef.nativeElement.querySelector('.canvas-container')
+      .addEventListener('mouseover', (event: MouseEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onMouseOver(event);
+        });
+      });
+
+    this.elementRef.nativeElement.querySelector('.canvas-container')
+      .addEventListener('mouseleave', (event: MouseEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onMouseLeave(event);
+        });
+      });
+
+    window
+      .addEventListener('keydown', (event: KeyboardEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onKeyDown(event);
+        });
+      });
+
+    window
+      .addEventListener('keyup', (event: KeyboardEvent) => {
+        this.ngZone.runOutsideAngular( () => {
+          this.fabricService.onKeyUp(event);
+        });
+      });
+
+    this.fabricService.drawGrid();
+  }
 
   public generateSeatMap(): void {
     const map: OptSeat[][] = [];
@@ -63,5 +146,14 @@ export class DesignerComponent implements OnInit{
     this.layoutService.saveLayout(body).subscribe(data => {
       console.log('[Debug] Data:', data);
     });
+  }
+
+  generateSeats(): void {
+    this.fabricService.generateSeats(this.rowMetric.count, this.colMetric.count, this.colMetric.distance, this.rowMetric.distance, 40);
+  }
+
+  toggleSnapToGrid(): void {
+    console.log(this.snapToGrid);
+    this.fabricService.SnapToGrid = this.snapToGrid;
   }
 }
